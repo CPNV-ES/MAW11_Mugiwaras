238ad3e5ed2e31309ae5bb83e26078fae30e8b80
Benjamin Fontana <ben192003@gmail.com>
2023-11-15 10:38:10 +0100

    updated namespace definition to fit composer requirement

--- app/core/View.php
+++ app/core/View.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Mugiwaras\Framework\Core;
 
 class View
 {
--- index.php
+++ index.php
@@ -4,4 +4,5 @@ require __DIR__ . '/vendor/autoload.php';
 
 Dotenv\Dotenv::createUnsafeImmutable(__DIR__)->load();
 
-App\Core\Router::get('/images/{id}/create/{img}', 'Controller@index');
+Mugiwaras\Framework\Core\Router::get('/images/{id}/create/{img}', 'Controller@index');
+Mugiwaras\Framework\Core\Router::post('/images/{id}/create/{img}', 'Controller@create');
