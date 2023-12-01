1d73af88f665e47b3b64c75420e6e7d51a4c9653
Benjamin Fontana <ben192003@gmail.com>
2023-12-01 13:07:44 +0100

    Updated the Request & Router class to handle DELETE, PUT & PATCH
    * changed the $body property from public readonly -> private
    * added getter for body
    * added setRequestMethod()
    * added getCustomRequestMethod()

--- app/core/Request.php
+++ app/core/Request.php
@@ -6,14 +6,18 @@ class Request
 {
 
     public readonly String $uri;
+    private mixed $body;
     public readonly String $method;
-    public readonly mixed $body;
 
     public function __construct()
     {
         $this->uri = $_SERVER['REQUEST_URI'];
-        $this->method = $_SERVER['REQUEST_METHOD'];
         $this->body = $this->mapRequestContent(file_get_contents('php://input'));
+        $this->method = $this->setRequestMethod();
+    }
+
+    public function body(){
+        return $this->body;
     }
 
     /**
@@ -33,5 +37,37 @@ class Request
         }
 
         return $content;
+    }
+    
+    /**
+     * Returns the request method.
+     *
+     * @return string
+     */
+    private function setRequestMethod()
+    {
+        $method = $this->getCustomRequestMethod($this->body);
+
+        if ($method) {
+            return $method;
+        } else {
+            return $_SERVER['REQUEST_METHOD'];
+        }
+    }
+        
+    /**
+     * Returns the request method PUT, DELETE or any other based on the body attribute "_method" or returns null if no "_method" is found.
+     *
+     * @param  mixed $body
+     * @return string|null
+     */
+    private function getCustomRequestMethod($body)
+    {
+        if (isset($body['_method'])) {
+            $method = strtoupper($body['_method']);
+            unset($this->body['_method']);
+            return $method;
+        }
+        return null;
     }
 }
