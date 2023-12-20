12b22e0c65654f38853a3e5d13466c8f3ffc5828
Benjamin Fontana <ben192003@gmail.com>
2023-12-20 10:10:15 +0100

    Added chainable method where()

--- app/core/QueryBuilder.php
+++ app/core/QueryBuilder.php
@@ -38,8 +38,30 @@ class QueryBuilder
     public function get()
     {
         $query = "SELECT * FROM " . $this->tableName;
+        $query = $this->prepareWhereClause($query, $this->whereClauses);
         $stmt = $this->pdo->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function where($column, $operator, $value)
+    {
+        array_push($this->whereClauses, addSlashes($column) . " " . addSlashes($operator) . " '" . addSlashes($value) . "'");
+        return $this;
+    }
+
+    private function prepareWhereClause($query)
+    {
+        if (!empty($this->whereClauses)) {
+            $query .= " WHERE ";
+            foreach ($this->whereClauses as $key => $whereClause) {
+                $query .= " " . $whereClause;
+                if ($key != count($this->whereClauses) - 1) {
+                    $query .= " AND ";
+                }
+            }
+            $this->whereClauses = [];
+        }
+        return $query;
     }
 }
