8e3c43200aefc3bdd977e079da6fe365f13c2d84
Benjamin Fontana <ben192003@gmail.com>
2023-12-23 23:36:56 +0100

    refactored & commented QueryBuilder
    * removed idPrefix
    * added comment to all public method
    * added check() to run checker when calling a method
    * added execute()
    * save() refactored to use a named key array
    * Typed the method parameters

--- app/core/QueryBuilder.php
+++ app/core/QueryBuilder.php
@@ -7,13 +7,15 @@ use PDO;
 
 class QueryBuilder
 {
-    private static $instance = null;
-    private string $idPrefix = "id_";
     private $pdo;
 
+    private static $instance = null;
+
     private string $tableName;
+
     private array $whereClauses = [];
     private array $orderClauses = [];
+
     private string $limitQuery = "";
     private string $joinQuery = "";
 
@@ -24,6 +26,11 @@ class QueryBuilder
         $this->pdo = new PDO("mysql:host=" . getenv("DB_HOST") . ";dbname=" . getenv("DB_DATABASE"), getenv("DB_USERNAME"), getenv("DB_PASSWORD"));
     }
 
+    /**
+     * get the instance of QueryBuilder
+     *
+     * @return QueryBuilder
+     */
     public static function getInstance(): QueryBuilder
     {
         if (self::$instance == null) {
@@ -33,15 +40,30 @@ class QueryBuilder
         return self::$instance;
     }
 
-    public function table($name)
+    /**
+     * registers table name to be used in query
+     *
+     * @param  string $name
+     * @return object
+     */
+    public function table(string $name)
     {
         $this->tableName = $name;
         return $this;
     }
 
-    public function get(array $columns = ['*'])
+    /**
+     * gets all rows from table with specified columns. If parameters are not specified, all columns are returned. this method is chainable and must be called as the last method in the chain.
+     *
+     * @param  array $columns
+     * @return array|bool
+     */
+    public function get(array $columns = ['*']): array|bool
     {
+        $this->check(['table']);
+
         $this->query = "SELECT ";
+
         foreach ($columns as $key => $column) {
             if ($key > 0) {
                 $this->query .= ",";
@@ -53,56 +75,84 @@ class QueryBuilder
         }
 
         $this->query .= $this->joinQuery;
-        $this->prepareClause($this->whereClauses, "WHERE", function ($clause) {
+
+        $this->prepareClause($this->whereClauses, "WHERE", function (QueryClause $clause) {
             return $clause->getType();
         });
         $this->prepareClause($this->orderClauses, "ORDER BY", " ");
+
         $this->query .= $this->limitQuery;
 
         $this->joinQuery = "";
         $this->limitQuery = "";
 
-        $stmt = $this->pdo->prepare($this->query);
-        print_r($stmt);
-        die();
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $this->execute()->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    // todo refactor to accept key named array as paramerters instead of two arrays
-    public function save($columns, $values)
+    /**
+     * inserts a row into the table
+     *
+     * @param  array $columns
+     * @param  array $values
+     * @return void
+     */
+    public function save(array $insertSet)
     {
+        $this->check(['table']);
+
+        $columns = array_keys($insertSet);
+        $values = array_values($insertSet);
+
         $clause = new QueryClause($columns, null, $values);
+
         $query = "INSERT INTO " . $this->tableName . " (" . $clause->getColumns() . ") VALUES (" . $clause->getValues() . ")";
+
         $this->query = $query;
-        $stmt = $this->pdo->prepare($this->query);
-        $stmt->execute();
+        $this->execute();
+
         return $this->pdo->lastInsertId();
     }
 
+    /**
+     * Deletes rows specified with the where method from table
+     *
+     * @return void
+     */
     public function delete()
     {
+        $this->check(['table', 'where']);
+
         $this->query = "DELETE FROM " . $this->tableName;
+
         $this->prepareClause($this->whereClauses, "WHERE");
-        $stmt = $this->pdo->prepare($this->query);
-        $stmt->execute();
-        return $stmt->rowCount();
+
+        return $this->execute()->rowCount();
     }
 
+    /**
+     * updates rows specified with the where method and with the values specified in the parameter
+     *
+     * @param  array $updateSet
+     * @return void
+     */
     public function update(array $updateSet)
     {
+        $this->check(['table', 'where']);
+
         $this->query = "UPDATE " . $this->tableName . " SET ";
+
         $index = array_flip(array_keys($updateSet));
+
         foreach ($updateSet as $key => $value) {
             $this->query .= $key . '="' . $value . '"';
             if ($index[$key] < count($updateSet) - 1) {
                 $this->query .= ", ";
             }
         }
+
         $this->prepareClause($this->whereClauses, "WHERE");
-        $stmt = $this->pdo->prepare($this->query);
-        $stmt->execute();
-        return $stmt->rowCount();
+
+        return $this->execute()->rowCount();
     }
 
     private function addWhereClauseToArray(QueryClause $whereClause)
@@ -111,13 +161,30 @@ class QueryBuilder
         return $this;
     }
 
-    public function where($column, $operator, $value, $type = "AND")
+    /**
+     * adds a (AND) where clause to the query
+     *
+     * @param  string $column
+     * @param  string $operator
+     * @param  string $value
+     * @param  string $type
+     * @return void
+     */
+    public function where(string $column, string $operator, string $value, string $type = "AND")
     {
         $this->addWhereClauseToArray(new QueryClause($column, $operator, $value, $type));
         return $this;
     }
 
-    public function orWhere($column, $operator, $value)
+    /**
+     * adds an or where clause to the query
+     *
+     * @param  string $column
+     * @param  string $operator
+     * @param  string $value
+     * @return void
+     */
+    public function orWhere(string $column, string $operator, string $value)
     {
         $this->where($column, $operator, $value, "OR");
         return $this;
@@ -128,39 +195,69 @@ class QueryBuilder
         array_push($this->orderClauses, $orderClause);
     }
 
+    /**
+     * orders the query DESC by the specified columns in descending order
+     *
+     * @param  array|string $columns
+     * @return void
+     */
     public function desc(array|string $columns)
     {
         $this->addOrderClauseToArray(new QueryClause($columns, "DESC"));
         return $this;
     }
 
+    /**
+     * orders the query ASC by the specified columns in ascending order
+     *
+     * @param  array|string $columns
+     * @return void
+     */
     public function asc(array|string $columns)
     {
         $this->addOrderClauseToArray(new QueryClause($columns, "ASC"));
         return $this;
     }
 
-    public function limit($limit, $offset = 0)
+    /**
+     * limits the query to the specified number of rows and starts from the specified offset
+     *
+     * @param  string $limit
+     * @param  int $offset
+     * @return void
+     */
+    public function limit(string $limit, int $offset = 0)
     {
         if ($offset > 0) {
             $this->limitQuery .= " LIMIT " . $limit . " OFFSET " . $offset;
             return $this;
         }
+
         $this->limitQuery .= " LIMIT " . $limit;
+
         return $this;
     }
 
-    public function join($table, $extIdName, $intIdName)
+    /**
+     * joins the specified table with the specified id names
+     *
+     * @param  string $table
+     * @param  string $extIdName
+     * @param  string $intIdName
+     * @return void
+     */
+    public function join(string $table, string $extIdName, string $intIdName)
     {
         $this->joinQuery .= " JOIN " . $table . " ON " . $extIdName . " = " . $intIdName;
         return $this;
     }
 
-    private function prepareClause($clauses, $keyword, $separator = ",")
+    private function prepareClause(array $clauses, string $keyword, $separator = ",")
     {
         if (count($clauses) == 0) {
             return;
         }
+
         $this->query .= " " . $keyword;
 
         foreach ($clauses as $key => $clause) {
@@ -173,6 +270,41 @@ class QueryBuilder
             }
             $this->query .= " " . $clause;
         }
+
         $clauses = array();
+    }
+
+    private function execute()
+    {
+        $stmt = $this->pdo->prepare($this->query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    private function checkIfWhereClausesAreSet()
+    {
+        if (count($this->whereClauses) == 0) {
+            throw new \Exception("No where clause specified");
+        }
+    }
+
+    private function checkIfTableNameIsSet()
+    {
+        if ($this->tableName == "" || $this->tableName == null) {
+            throw new \Exception("No table name specified");
+        }
+    }
+
+    private function check(array $checkers = [])
+    {
+        if (count($checkers) == 0) {
+            return;
+        }
+        if ($checkers['table']) {
+            $this->checkIfTableNameIsSet();
+        }
+        if ($checkers['where']) {
+            $this->checkIfWhereClausesAreSet();
+        }
     }
 }
