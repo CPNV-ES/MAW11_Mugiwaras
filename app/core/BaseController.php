2c19ce4f4b2f92a4a817c6dea30c0ecfce554c2a
Benjamin Fontana <ben192003@gmail.com>
2023-12-23 23:56:52 +0100

    added constructor in BaseController

--- app/core/BaseController.php
+++ app/core/BaseController.php
@@ -2,7 +2,18 @@
 
 namespace Mugiwaras\Framework\Core;
 
+use Mugiwaras\Framework\Core\Renderer;
+
+use Mugiwaras\Framework\Core\QueryBuilder;
+
 abstract class BaseController
 {
-    abstract public function index($msg = []);
+    protected Renderer $renderer;
+    protected QueryBuilder $qb;
+
+    public function __construct()
+    {
+        $this->renderer = new Renderer(dirname(__DIR__) . '/views');
+        $this->qb = QueryBuilder::getInstance();
+    }
 }
