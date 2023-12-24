1b8e875f18488e0f01a5c22bd29e0f82468fe0bd
Benjamin Fontana <ben192003@gmail.com>
2023-12-24 17:24:58 +0100

    cleaned up the project

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-Dotenv\Dotenv::createUnsafeImmutable(__DIR__)->load();
-
-Mugiwaras\Framework\Core\Router::get('/images/{id}/create/{img}', 'Controller@index');
-Mugiwaras\Framework\Core\Router::post('/images/{id}/create/{img}', 'Controller@create');
